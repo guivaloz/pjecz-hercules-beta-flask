786011bf6727636eed854edf15586036f0482973
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2026-06-20 19:40:06 -0600

    Valor por defecto en Bitácoras de APIs

--- pjecz_hercules_beta_flask/blueprints/bitacoras_apis/models.py
+++ pjecz_hercules_beta_flask/blueprints/bitacoras_apis/models.py
@@ -35,10 +35,10 @@ class BitacoraAPI(database.Model, UniversalMixin):
     api_nombre: Mapped[str] = mapped_column(String(256))
     api_ruta: Mapped[str] = mapped_column(String(512))
     peticion: Mapped[str] = mapped_column(Enum(*PETICIONES, name="peticiones_tipos", native_enum=False), index=True)
-    respuesta_exitosa: Mapped[Optional[bool]] = mapped_column(Boolean)
-    respuesta_mensaje: Mapped[Optional[str]] = mapped_column(String(256))
-    respuesta_errores: Mapped[list[str]] = mapped_column(JSON)
-    respuesta_datos: Mapped[list[dict]] = mapped_column(JSON)
+    respuesta_exitosa: Mapped[Optional[bool]] = mapped_column(Boolean, default=None)
+    respuesta_mensaje: Mapped[Optional[str]] = mapped_column(String(256), default="")
+    respuesta_errores: Mapped[list[str]] = mapped_column(JSON, default=[])
+    respuesta_datos: Mapped[list[dict]] = mapped_column(JSON, default=[])
 
     def __repr__(self):
         """Representación"""
